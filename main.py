c1222fa434019a34591daae26a197f9411766603
Shashi Prakash <sspeedysingh096@gmail.com>
2021-03-25 12:18:35 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+''' Python Assignment for splitting the file based on size - Submitted by - Shashi Prakash 
+    input format -> python main.py input.csv '''
+
+import sys
+from fsplit.filesplit import Filesplit
+
+fs = Filesplit()
+
+#Callback function to print the split outputs in the terminal 
+def split_cb(f, s):
+    print("file: {0}, size: {1}".format(f, s))
+
+#file -> input file name
+#split_size -> size of every splits in bytes
+#output_dir -> target directory to store the splits.
+fs.split(file=sys.argv[1], split_size=1000000, output_dir="/home/shashi/Assignmnet/Splits", callback=split_cb)
