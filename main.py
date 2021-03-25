308a55d3a014c8f0e7bf40ad4d889b3182e5fc96
Shashi Prakash <sspeedysingh096@gmail.com>
2021-03-25 12:24:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ''' Python Assignment for splitting the file based on size - Submitted by - Shashi Prakash 
-    input format -> python main.py input.csv '''
+    input format -> python main.py input.csv 
+    input.csv is majestic million csv file, which contains recods over a million for testing purpose'''
 
 import sys
 from fsplit.filesplit import Filesplit
@@ -14,3 +15,4 @@ def split_cb(f, s):
 #split_size -> size of every splits in bytes
 #output_dir -> target directory to store the splits.
 fs.split(file=sys.argv[1], split_size=1000000, output_dir="/home/shashi/Assignmnet/Splits", callback=split_cb)
+
